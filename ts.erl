05b4121c763f952710de271374c9a56ddfb3f60d
Fabian Stolz <stolz@ed3507-12.ed3507.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-09-22 11:52:07 +0200

    implemented in and out, refined guards in server

--- ts.erl
+++ ts.erl
@@ -2,20 +2,27 @@
 -export([new/0, in/2, out/2]).
 
 new() ->
-  0.
+  spawn_link(fun() -> server([],[]) end).
 
+% client wants to read in a tuple
 in(TS, Pattern) ->
-  0.
+  TS ! {self(), Pattern},
+  receive
+    Tuple -> io:fwrite("Process ~p received Tuple ~p from Tuplespace ~p~n", self(), Tuple, TS)
+  end.
 
+% client emits a tuple
 out(TS, Tuple) ->
-  0.
+  TS ! Tuple,
+  io:fwrite("Process ~p sent Tuple ~p to Tuplespace ~p~n", self(), Tuple, TS).
 
-server(L) ->
+server(Tuples, Waitlist) ->
 
   receive
-    {Pid, Pattern} when matchList(Pattern, L) ->
-      Pid ! 0;
-    {Tuple} -> server(L ++ Tuple)
+    {Pid, Pattern} -> server(Tuples, Waitlist ++ [{Pid, Pattern}]);
+    %TODO: check if this process can be immediately woken
+    {Tuple} -> server(Tuples ++ [Tuple], Waitlist)
+    %TODO: go through waitlist and check if there's a process that can be woken
   end.
 
 matchList(Pattern, L) ->
@@ -24,7 +31,7 @@ matchList(Pattern, L) ->
      false -> matchList(Pattern, tl(L))
   end.
 
-% Match method from Erlang?
+% Match method
 match(any,_) -> true;
 match(P,Q) when is_tuple(P), is_tuple(Q)
                 -> match(tuple_to_list(P),tuple_to_list(Q));
