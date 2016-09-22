346f448aee7f1884b4742e6188551467b0db1ffb
Nancy Truong <truongh.nancy@gmail.com>
2016-09-22 09:53:23 +0200

    writing server

--- ts.erl
+++ ts.erl
@@ -9,3 +9,21 @@ in(TS, Pattern) ->
 
 out(TS, Tuple) ->
   0.
+
+server(L) ->
+  receive
+    {Pid, Pattern} when match(Pattern, L) ->
+      Pid ! 0;
+    {Tuple} -> server(L ++ Tuple)
+  end.
+
+% Match method from Erlang?
+  match(any,_) -> true;
+  match(P,Q) when is_tuple(P), is_tuple(Q)
+                  -> match(tuple_to_list(P),tuple_to_list(Q));
+  match([P|PS],[L|LS]) -> case match(P,L) of
+                                true -> match(PS,LS);
+                                false -> false
+                           end;
+  match(P,P) -> true;
+  match(,) -> false.
