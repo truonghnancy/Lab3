766660706deca55248f2b6a7edce498252c77fba
Fabian Stolz <stolz@ed3507-06.ed3507.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-09-29 10:04:32 +0200

    Wrote tests and added explanation

--- owntest.erl
+++ owntest.erl
@@ -2,6 +2,36 @@
 -export([test/0, slave/2]).
 -import(ts,[in/2,out/2,new/0]).
 
+%% Sample run
+%% (node@chalmers)4> c(ts).  
+%% {ok,ts}
+%% (node@chalmers)5> Pid = ts:new().
+%% <0.55.0>
+%% (node@chalmers)6> ts:out(Pid, {hello,world}).
+%% Process <0.32.0> sent {hello,world} to Tuplespace <0.55.0>
+%% ok
+%% (node@chalmers)7> ts:out(Pid, {hello,all}).
+%% Process <0.32.0> sent {hello,all} to Tuplespace <0.55.0>
+%% ok
+%% (node@chalmers)8> ts:out(Pid, {goodbye,all}).
+%% Process <0.32.0> sent {goodbye,all} to Tuplespace <0.55.0>
+%% ok
+%% (node@chalmers)9> spawn(ts,in,[Pid,{any,any,any}]).
+%% <0.60.0>
+%% (node@chalmers)10> spawn(ts,in,[Pid,{any,all}]).    
+%% Process <0.62.0> received {hello,all} from Tuplespace <0.55.0>
+%% <0.62.0>
+%% (node@chalmers)11> spawn(ts,in,[Pid,{any,all}]).
+%% Process <0.64.0> received {goodbye,all} from Tuplespace <0.55.0>
+%% <0.64.0>
+%% (node@chalmers)12> ts:out(Pid, {hello,all,again}).
+%% Process <0.32.0> sent {hello,all,again} to Tuplespace <0.55.0>
+%% Process <0.60.0> received {hello,all,again} from Tuplespace <0.55.0>
+%% ok
+%% (node@chalmers)13> spawn(ts,in,[Pid,{any,any}]).    
+%% Process <0.67.0> received {hello,world} from Tuplespace <0.55.0>
+%% <0.67.0>                              
+
 test() ->
     Delay = 500,
     process_flag(trap_exit, true),
@@ -9,15 +39,39 @@ test() ->
     link(TS),
     io:format("TEST: new tuplespace TS created~n", []),
 
-    Slave1 = spawn_in_test(TS, {fish,any}),
+    Slave1 = spawn_in_test(TS, {hello, world, any}),
+    sleep(Delay),
+
+    out_test(TS, {hello,fabian}),
+    sleep(Delay),
+
+    Slave2 = spawn_in_test(TS, {hello, fabian}),
+    sleep(Delay),
+
+    replytest(Slave2, {hello,fabian}, {hello,fabian}),
+    sleep(Delay),
+
+    out_test(TS, {hello, world, nancy}),
+    sleep(Delay),
+
+    replytest(Slave1, {hello, world, any}, {hello, world, nancy}),
+    sleep(Delay),
+
+    Slave3 = spawn_in_test(TS, {any,any}),
+    sleep(Delay),
+
+    Slave4 = spawn_in_test(TS, {any,any,any}),
     sleep(Delay),
 
     receive
-	{Slave4, Tup} ->
+	{Slave3, Tup} ->
+	    io:format("Error. Empty tuplespace, but received: ~w~n",[Tup]);
+        {Slave4, Tup} ->
 	    io:format("Error. Empty tuplespace, but received: ~w~n",[Tup])
     after
         1000 ->   
 	    io:format("Correct. Tuplespace appears to be empty.~n"),
+	    exit(Slave3, this_is_it),
 	    exit(Slave4, this_is_it),
 	    exit(TS, this_is_it),
 	    collect_exits([Slave1, Slave2, Slave3, Slave4, TS]),
